5208bc83d1d9454bc5f137c4b4df156ee61049ed
klau2000 <klau00@stuy.edu>
2019-11-26 17:43:41 -0500

    edits

--- parseargs.c
+++ parseargs.c
@@ -8,7 +8,7 @@
 
 char ** parse_args (char *line) {
   char ** ans;
-  ans = malloc ( 5 * sizeof(line));
+  ans = malloc (5 * sizeof(char *);
   char *curr = line;
   int count = 0;
   while (curr){
