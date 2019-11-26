6c3b71bc1edfbfba54178e4e5b71c3b4d44c69ae
klau2000 <klau00@stuy.edu>
2019-11-26 17:54:48 -0500

    Edits

--- parseargs.c
+++ parseargs.c
@@ -20,7 +20,7 @@ char ** parse_args (char *line) {
 
 int main () {
   char *line = strdup("ls -a -l");
-  printf("Using parse_args to run ls -a -l:"\n);
+  printf("Using parse_args to run ls -a -l:\n");
   char ** args = parse_args(line);
   execvp(args[0], args);
 }
